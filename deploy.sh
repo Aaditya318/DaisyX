04d157b09ee44ed46e7fa3ecdc01ff8f7b186892
TOBOT UPDATE <83446207+Aaditya318@users.noreply.github.com>
2021-07-20 09:39:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ echo "
 *********** STARTING DEPLOY ***********
 
 DAISYX v2 -Base Aiogram
-(C) 2020-2021 by @TEAMDAISYX
-Support Chat is @DAISYSUPPORT_OFFICIAL.
+(C) 2020-2021 by @rungram
+Support Chat is @run_gram.
 
 ***************************************
 "
@@ -37,8 +37,8 @@ DAISYX. Deployed Successfully
  ******************* v2.0 ** 
 
 Thanks for deploying DaisyX
-(C) 2020-2021 by @TEAMDAISYX
-Support Chat is @DAISYSUPPORT_OFFICIAL.
+(C) 2020-2021 by @rungram
+Support Chat is @rungram.
 =++---------------------------------------------++=
                        Greetings from dev team :)
     "
